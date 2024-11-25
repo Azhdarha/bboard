ec594bd00e33501887086bb5bc5e318e255f386b
Артур Кузбагаров <171016987@step.kz>
2024-11-25 17:17:02 +0500

    lesson_13 files

--- bboard/urls.py
+++ bboard/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path, re_path
 
-from bboard.views import index, by_rubric, BbCreateView
+from bboard.views import index, by_rubric, BbCreateView, add, add_save, add_and_save
 
 app_name = 'bboard'
 
 urlpatterns = [
-    path('add/', BbCreateView.as_view(), name='add'),
+    # path('add/', BbCreateView.as_view(), name='add'),
+    #path('add/save/', add_save, name='add_save'),
+    #path('add/', add, name='add'),
     path('<int:rubric_id>/', by_rubric, name='by_rubric'),
     path('', index, name='index'),
+
+    path('add/', add_and_save, name='add'),
 
     #re_path(r^'add/$', BbCreateView.as_view(), name='add'),
     #re_path(r'^(?P<int:rubric_id>[0-9]*)/$', by_rubric, name='by_rubric'),
--- bboard/views.py
+++ bboard/views.py
@@ -1,21 +1,38 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.template import loader
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic.edit import CreateView
 from django.db.models import Count
 
-#from bboard.forms import BbForm
+from bboard.forms import BbForm
 from bboard.models import Bb, Rubric
 
+# def index(request):
+#     bbs = Bb.objects.order_by('-published')
+#     rubrics = Rubric.objects.annotate(cnt=Count('bb')).filter(cnt__gt=0)
+#     context = {'bbs': bbs, 'rubrics': rubrics}
+#
+#     return render(request, 'bboard/index.html', context)
+
+# def index(request):
+#     resp = HttpResponse('Сдесь будет', content_type='text/plain; charset=utf-8')
+#     resp.write('лавная')
+#     resp.writelines(('страница','сайта'))
+#     resp['keywords'] = 'Python', 'Django'
+#     return resp
+
+# def index(request):
+#     bbs = Bb.objects.all()
+#     rubrics = Rubric.objects.all()
+#     context = {'bbs': bbs, 'rubtics': rubrics}
+#     from django.
+
 def index(request):
-    bbs = Bb.objects.order_by('-published')
-    rubrics = Rubric.objects.annotate(cnt=Count('bb')).filter(cnt__gt=0)
-    context = {'bbs': bbs, 'rubrics': rubrics}
-
-    return render(request, 'bboard/index.html', context)
-
-
+    bbs = Bb.objects.all()
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubtics': rubrics}
+    return HttpResponse(render_to_string())
 
 def by_rubric(request, rubric_id):
     bbs = Bb.objects.filter(rubric=rubric_id)
@@ -40,3 +57,35 @@ class BbCreateView(CreateView):
         context = super().get_context_data(**kwargs)
         context['rubrics'] = Rubric.objects.annotate(cnt=Count('bb')).filter(cnt__gt=0)
         return context
+
+
+def add(request):
+    bbf = BbForm()
+    context = {'form': bbf}
+    return render(request, 'bboard/bb_create.html', context)
+
+
+def add_save(request):
+    bbf = BbForm(request.POST)
+    if bbf.is_valid():
+        bbf.save()
+        return HttpResponseRedirect(reverse('bboard:by_rubric', kwargs={'rubric_id': bbf.cleaned_data['rubric'].pk}))
+    else:
+        context = {'form': bbf}
+        return render(request, 'bboard/bb_create.html', context)
+
+
+def add_and_save(request):
+    if request.method == 'POST':
+        bbf = BbForm(request.POST)
+        if bbf.is_valid():
+            bbf.save()
+            return HttpResponseRedirect(reverse('bboard:by_rubric', kwargs={'rubric_id': bbf.cleaned_data['rubric'].pk}))
+        else:
+            context = {'form': bbf}
+            return render(request, 'bboard/bb_create.html', context)
+    else:
+        bbf = BbForm()
+        context = {'form': bbf}
+        return render(request, 'bboard/bb_create.html', context)
+
